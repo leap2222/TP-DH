4aafd9d232b8d60e7639949c1a81ff6919410bda
Digital House <e.fraffo@gmail.com>
2018-08-23 18:30:48 -0300

    23/08

--- script/tpi_db.sql
+++ script/tpi_db.sql
@@ -15,6 +15,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+DROP DATABASE IF EXISTS tpi_db;
+CREATE DATABASE tpi_db;
+USE tpi_db;
+
+
 --
 -- Table structure for table `comments`
 --

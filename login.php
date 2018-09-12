3c00c9a333d93242a7e824255e756a0cc7e6bcfb
leap2222 <leap2222@gmail.com>
2018-09-12 17:20:43 -0300

    login..

--- login.php
+++ login.php
@@ -30,14 +30,25 @@
 
 <?php $TituloPagina = "Login"; include 'header.php'; ?>
 
-<form method="post" enctype="multipart/form-data">
-	<input class="form-control" type="text" name="email" placeholder="usuario" value="<?=$email?>">
-	<br>
- 	<input class="form-control" type="password" name="pass" placeholder="contraseña">
-  <label class="centrar"><input type="checkbox" name="recordar" checked> Recordar</label>
-  <button class="btn btn-primary" type="submit">ENTRAR</button>
-	<a href="recuperar.php" class="registrar">¿Olvidaste la contraseña?</a>
-	<a href="registracion.php" class="registrar">¿Sos nuevo? REGISTRATE!</a>
-</form>
+<div class="row justify-content-md-center">
+	<div class="col-sm-6">
+		<form method="post" enctype="multipart/form-data">
+			<input class="form-control" type="text" name="email" placeholder="usuario" value="<?=$email?>">
+			<br>
+ 			<input class="form-control" type="password" name="pass" placeholder="contraseña">
+			<br>
+			<div class="row justify-content-md-center">
+				<div class="col-sm-6">
+  				<button class="btn btn-primary" type="submit">ENTRAR</button>
+				</div>
+				<div class="col-sm-6">
+					<label class="centrar"><input type="checkbox" name="recordar" checked> Recordar</label>
+				</div>
+			</div>
+			<a href="recuperar.php" class="registrar">¿Olvidaste la contraseña?</a>
+			<a href="registracion.php" class="registrar">¿Sos nuevo? REGISTRATE!</a>
+		</form>
+	</div>
+</div>
 
 <?php include 'footer.php' ?>
